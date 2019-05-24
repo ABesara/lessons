279fe8a4112854b8f84efd31759775466fc17af4
ABesara <alexbessarab@gmail.com>
2019-05-24 17:47:52 +0300

    Lesson 1 complete

--- hometask1.js
+++ hometask1.js
@@ -1,11 +1,19 @@
 // 1. объявить переменные user и password
-
+var user;
+var password;
 // 2. вывести с помощью console.log() значение переменной user
-
+console.log(user);
 // 3. вывести с помощью console.log() значение переменной password
-
+console.log(password);
 // 4. инициализировать пермеенную user произвольным значением
-
+user="Alex";
 // 5. инициализировать переменную password произвольным значением
-
+password="password123";
 // 6. вывести значения новых переменных на консоль
+console.log(user);
+console.log(password);
+user=password;
+console.log(user);
+user = password = 'newpassword';
+console.log(user);
+console.log(password);
